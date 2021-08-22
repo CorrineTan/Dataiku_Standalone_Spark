533f3d884ea3471acced080ddb01b7014da23581
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-08-22 22:03:46 +1000

    fix path

--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@ if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
 	# Initialize new data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
 	"$DSS_DATADIR"/bin/dssadmin install-R-integration
-	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome $SPARK_HOME -standaloneArchive $DKU_DIR/$SPARK_ARCHIVE
+	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome /opt/spark -standaloneArchive $DKU_DIR/$SPARK_ARCHIVE
 	echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties
 
 elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLDIR"') != "$DSS_INSTALLDIR" ]; then
 	# Upgrade existing data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -u -y
 	"$DSS_DATADIR"/bin/dssadmin install-R-integration
-	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome $SPARK_HOME -standaloneArchive $DKU_DIR/$SPARK_ARCHIVE
+	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome /opt/spark -standaloneArchive $DKU_DIR/$SPARK_ARCHIVE
 
 fi
 
