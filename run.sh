1982ac8098d36563907a26134806f4a1c7792289
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-08-22 21:39:30 +1000

    fix path error

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 DSS_INSTALLDIR="/home/dataiku/dataiku-dss-$DSS_VERSION"
+DSS_DATADIR="/home/dataiku/dss"
 SPARK_HOME="/opt/spark"
 SPARK_ARCHIVE="dataiku-dss-spark-standalone-8.0.7-2.4.5-generic-hadoop3.tar.gz"
 
@@ -8,18 +9,15 @@ if [ ! -f "$DSS_DATADIR"/bin/env-default.sh ]; then
 	# Initialize new data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -p "$DSS_PORT"
 	"$DSS_DATADIR"/bin/dssadmin install-R-integration
-	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome "$SPARK_HOME" -standaloneArchive $SPARK_ARCHIVE
+	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome "$SPARK_HOME" -standaloneArchive $DSS_DATADIR/$SPARK_ARCHIVE
 	echo "dku.registration.channel=docker-image" >>"$DSS_DATADIR"/config/dip.properties
 
 elif [ $(bash -c 'source "$DSS_DATADIR"/bin/env-default.sh && echo "$DKUINSTALLDIR"') != "$DSS_INSTALLDIR" ]; then
 	# Upgrade existing data directory
 	"$DSS_INSTALLDIR"/installer.sh -d "$DSS_DATADIR" -u -y
 	"$DSS_DATADIR"/bin/dssadmin install-R-integration
-	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome "$SPARK_HOME" -standaloneArchive $SPARK_ARCHIVE
+	"$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome "$SPARK_HOME" -standaloneArchive "$DSS_DATADIR"/$SPARK_ARCHIVE
 
 fi
-
-# wait a few sec
-sleep 15
 
 exec "$DSS_DATADIR"/bin/dss run
