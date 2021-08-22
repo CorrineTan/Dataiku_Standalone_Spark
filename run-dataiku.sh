c723d3d2e1c702f0df8ba3b6a7bc5a8c4434d1ee
Corrine Tenglun Tan <tenglunt@gmail.com>
2021-08-23 02:33:14 +1000

    update echo info

--- run-dataiku.sh
+++ run-dataiku.sh
@@ -5,25 +5,21 @@ DSS_DATADIR="/home/dataiku/dss"
 DKU_DIR="/home/dataiku"
 SPARK_ARCHIVE="dataiku-dss-spark-standalone-8.0.7-2.4.5-generic-hadoop3.tar.gz"
 
-# Let dataiku create /home/dataiku/dss folder first
 echo "Running DSS Now!"
 "$DKU_DIR"/run.sh &
 
-# Wait for everything's setup
-sleep 10
 echo "Waiting for everything setting up"
+sleep 10
 
-# Stop DSS 
-"$DSS_DATADIR"/bin/dss stop
 echo "DSS stop now"
+"$DSS_DATADIR"/bin/dss stop
 
-# Setup Spark Integration 
+echo "Setting up Spark Standalone Integration"
 "$DSS_DATADIR"/bin/dssadmin install-spark-integration -standaloneArchive "$DKU_DIR"/"$SPARK_ARCHIVE"
 
 # Wait for spark setup
 sleep 10
 
-# Start DSS 
 "$DSS_DATADIR"/bin/dss start
 echo "DSS restart now"
 
